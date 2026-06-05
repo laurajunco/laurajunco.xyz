fb64bcf0062ce698fac45c6f5dae00237947b5d9
Laura <lauracjunco@gmail.com>
2026-06-05 18:17:37 +0200

    migrate projects (wip)

--- .eleventy.js
+++ .eleventy.js
@@ -1,18 +1,19 @@
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("style.css");
   eleventyConfig.addPassthroughCopy("main.js");
   eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.addPassthroughCopy("CNAME");
   eleventyConfig.addPassthroughCopy("robots.txt");
+  eleventyConfig.addPassthroughCopy("work/*/assets");
 
   return {
     dir: {
       input: ".",
       includes: "_includes",
       layouts: "_layouts",
-      output: "_site"
+      output: "_site",
     },
     passthroughFileCopy: true,
-    templateFormats: ["html", "md", "njk"]
+    templateFormats: ["html", "md", "njk"],
   };
 };
