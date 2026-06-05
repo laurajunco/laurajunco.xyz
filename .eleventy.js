b581c0bc8966fe74e2df50fe0539049e0d4d75c7
Laura <lauracjunco@gmail.com>
2026-06-05 19:10:06 +0200

    add blogs collection

--- .eleventy.js
+++ .eleventy.js
@@ -6,11 +6,34 @@ module.exports = function (eleventyConfig) {
     return md.render(value || "");
   });
 
+  eleventyConfig.addNunjucksFilter("dateFilter", function (dateStr) {
+    const date = new Date(dateStr);
+    const options = { year: "numeric", month: "short", day: "2-digit" };
+    return date.toLocaleDateString("en-US", options);
+  });
+
   eleventyConfig.addPassthroughCopy("style.css");
   eleventyConfig.addPassthroughCopy("main.js");
   eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.addPassthroughCopy("CNAME");
   eleventyConfig.addPassthroughCopy("robots.txt");
+
+  eleventyConfig.addCollection("blog", function (collection) {
+    // Get all blog posts from blog/*.md files (11ty discovers them natively)
+    const posts = collection
+      .getFilteredByGlob("blog/*.md")
+      .filter((post) => post.fileSlug !== "index")
+      .map((post) => ({
+        date: post.data.date,
+        title: post.data.title,
+        type: post.data.externalUrl ? "external" : "local",
+        url: post.data.externalUrl || post.url,
+        content: post.content,
+      }));
+
+    // Sort by date descending
+    return posts.sort((a, b) => new Date(b.date) - new Date(a.date));
+  });
 
   return {
     dir: {
---
+++ blog/vizknowledge-2025-recap.md
@@ -0,0 +1,7 @@
+---
+layout: blog.njk
+navActive: blog
+title: "LAURA JUNCO - VizKnowledge 2025 recap"
+date: 2025-12-02
+externalUrl: "https://careers.celonis.com/blog/the-evolving-role-of-data-visualization-3-key-takeaways-for-practitioners"
+---
