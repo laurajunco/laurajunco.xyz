e716780a39d39dbf14d4d7ec71a23579a20d5f9e
Laura <lauracjunco@gmail.com>
2026-06-05 18:02:24 +0200

    initial setup

---
+++ .eleventy.js
@@ -0,0 +1,18 @@
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("style.css");
+  eleventyConfig.addPassthroughCopy("main.js");
+  eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.addPassthroughCopy("CNAME");
+  eleventyConfig.addPassthroughCopy("robots.txt");
+
+  return {
+    dir: {
+      input: ".",
+      includes: "_includes",
+      layouts: "_layouts",
+      output: "_site"
+    },
+    passthroughFileCopy: true,
+    templateFormats: ["html", "md", "njk"]
+  };
+};
